49962a117497fd2cd534346f979c1bfada49b7ed
Wanderlee Max <wanderleedev@gmail.com>
2023-07-17 17:24:04 -0500

    logo de btn arreglado

--- src/components/BtnRefresh.ts
+++ src/components/BtnRefresh.ts
@@ -1,6 +1,7 @@
+import logoBtn from '../img/svg/refresh.svg'
 const BtnRefresh = `
   <button id="btnRefresh" class="bg-slate-200 grid grid-cols-2 items-center py-4 px-8 rounded-lg justify-self-center active:bg-slate-400 duration-150">
-  <img class="w-10" src="./src/img/svg/refresh.svg" alt="">
+  <img class="w-10" src=${logoBtn} alt="">
   <p class="font-medium tracking-widest uppercase">Refresh</p>
   </button>
 `;
